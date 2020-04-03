b9b178e216135caa0dcc66a34e5e3b5bca81f293
mu88 <4560672+mu88@users.noreply.github.com>
2020-04-03 11:28:41 +0200

    Set path base and remove more template code

--- RaspiFanController/Startup.cs
+++ RaspiFanController/Startup.cs
@@ -45,6 +45,8 @@ namespace RaspiFanController
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UsePathBase("/cool");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
