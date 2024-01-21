d1babb836cc8dc88d16e598c863b63b1eb54bbf0
mu88 <4560672+mu88@users.noreply.github.com>
2024-01-21 15:19:29 +0100

    feat: use built-in temperature provider

---
+++ RaspiFanController/Logic/RaspiTemperatureProvider2.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Iot.Device.CpuTemperature;
+
+namespace RaspiFanController.Logic;
+
+[ExcludeFromCodeCoverage]
+public class RaspiTemperatureProvider2(ILogger<RaspiTemperatureProvider2> logger) : ITemperatureProvider
+{
+    /// <inheritdoc />
+    public (double, string) GetTemperature()
+    {
+        var cpuTemperature = new CpuTemperature();
+        var temperatureObject = cpuTemperature.ReadTemperatures().First();
+        return !double.IsNaN(temperatureObject.Temperature.DegreesCelsius) ? (temperatureObject.Temperature.DegreesCelsius, "C") : (double.NaN, "#");
+    }
+
+    /// <inheritdoc />
+    public bool IsPlatformSupported()
+    {
+        var isPlatformSupported = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        logger.LogDebug("Is platform supported: {IsPlatformSupported}", isPlatformSupported);
+        return isPlatformSupported;
+    }
+}
