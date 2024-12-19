642e78a3b76186074beb8d45a08f0883aa5e9c6e
mu88 <4560672+mu88@users.noreply.github.com>
2024-12-19 16:56:13 +0100

    feat: embed health check tool

--- Tests/System/SystemTests.cs
+++ Tests/System/SystemTests.cs
@@ -14,26 +14,33 @@ public class SystemTests
     public async Task AppRunningInDocker_ShouldBeHealthy()
     {
         // Arrange
-        await BuildDockerImageOfAppAsync();
-        var container = await StartAppInContainersAsync();
+        CancellationToken cancellationToken = CreateCancellationToken(TimeSpan.FromMinutes(1));
+        await BuildDockerImageOfAppAsync(cancellationToken);
+        var container = await StartAppInContainersAsync(cancellationToken);
         var httpClient = new HttpClient { BaseAddress = GetAppBaseAddress(container) };
 
         // Act
-        var healthCheckResponse = await httpClient.GetAsync("healthz");
-        var appResponse = await httpClient.GetAsync("/");
+        var healthCheckResponse = await httpClient.GetAsync("healthz", cancellationToken);
+        var appResponse = await httpClient.GetAsync("/", cancellationToken);
+        ExecResult healthCheckToolResult = await container.ExecAsync(["dotnet", "/app/mu88.HealthCheck.dll", "http://localhost:8080/cool/healthz"], cancellationToken);
 
         // Assert
-        var logValues = await container.GetLogsAsync();
-        Console.WriteLine($"Stderr:{Environment.NewLine}{logValues.Stderr}");
-        Console.WriteLine($"Stdout:{Environment.NewLine}{logValues.Stdout}");
-        logValues.Stdout.Should().NotContain("warn:");
-        healthCheckResponse.Should().BeSuccessful();
-        (await healthCheckResponse.Content.ReadAsStringAsync()).Should().Be("Healthy");
-        appResponse.Should().BeSuccessful();
-        (await appResponse.Content.ReadAsStringAsync()).Should().Contain("<title>Raspi Fan Controller</title>");
+        await LogsShouldNotContainWarningsAsync(container, cancellationToken);
+        await HealthCheckShouldBeHealthyAsync(healthCheckResponse, cancellationToken);
+        await AppShouldRunAsync(appResponse, cancellationToken);
+        healthCheckToolResult.ExitCode.Should().Be(0);
     }
 
-    private static async Task BuildDockerImageOfAppAsync()
+    private static CancellationToken CreateCancellationToken(TimeSpan timeout)
+    {
+        var timeoutCts = new CancellationTokenSource();
+        timeoutCts.CancelAfter(timeout);
+        CancellationToken cancellationToken = timeoutCts.Token;
+        
+        return cancellationToken;
+    }
+
+    private static async Task BuildDockerImageOfAppAsync(CancellationToken cancellationToken)
     {
         var rootDirectory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent ?? throw new NullReferenceException();
         var projectFile = Path.Join(rootDirectory.FullName, "RaspiFanController", "RaspiFanController.csproj");
@@ -42,7 +49,7 @@ public class SystemTests
             StartInfo = new ProcessStartInfo
             {
                 FileName = "dotnet",
-                Arguments = $"publish {projectFile} --os linux --arch amd64 /t:PublishContainer -p:ContainerImageTags=local-system-test",
+                Arguments = $"publish {projectFile} --os linux --arch amd64 /t:PublishContainer -p:ContainerFamily=noble-chiseled -p:ContainerImageTags=local-system-test-chiseled",
                 UseShellExecute = false,
                 RedirectStandardOutput = true,
                 CreateNoWindow = true
@@ -51,23 +58,23 @@ public class SystemTests
         process.Start();
         while (!process.StandardOutput.EndOfStream)
         {
-            Console.WriteLine(await process.StandardOutput.ReadLineAsync());
+            Console.WriteLine(await process.StandardOutput.ReadLineAsync(cancellationToken));
         }
 
-        await process.WaitForExitAsync();
+        await process.WaitForExitAsync(cancellationToken);
         process.ExitCode.Should().Be(0);
     }
 
-    private static async Task<IContainer> StartAppInContainersAsync()
+    private static async Task<IContainer> StartAppInContainersAsync(CancellationToken cancellationToken)
     {
         Console.WriteLine("Building and starting network");
-        var network = new NetworkBuilder().Build();
-        await network.CreateAsync();
+        INetwork? network = new NetworkBuilder().Build();
+        await network.CreateAsync(cancellationToken);
         Console.WriteLine("Network started");
 
         Console.WriteLine("Building and starting app container");
-        var container = BuildAppContainer(network);
-        await container.StartAsync();
+        IContainer container = BuildAppContainer(network);
+        await container.StartAsync(cancellationToken);
         Console.WriteLine("App container started");
 
         return container;
@@ -75,12 +82,34 @@ public class SystemTests
 
     private static IContainer BuildAppContainer(INetwork network) =>
         new ContainerBuilder()
-            .WithImage("mu88/raspifancontroller:local-system-test")
+            .WithImage("mu88/raspifancontroller:local-system-test-chiseled")
             .WithNetwork(network)
             .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development") // this enables the faked temperature and fan controller as we're not on a real Raspi
             .WithPortBinding(8080, true)
-            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(8080))
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilMessageIsLogged(
+                "Content root path: /app",
+                strategy => strategy.WithTimeout(TimeSpan.FromSeconds(30)))) // as it's a chiseled container, waiting for the port does not work
             .Build();
 
     private static Uri GetAppBaseAddress(IContainer container) => new($"http://{container.Hostname}:{container.GetMappedPublicPort(8080)}/cool");
+
+    private static async Task AppShouldRunAsync(HttpResponseMessage appResponse, CancellationToken cancellationToken)
+    {
+        appResponse.Should().BeSuccessful();
+        (await appResponse.Content.ReadAsStringAsync(cancellationToken)).Should().Contain("<title>Raspi Fan Controller</title>");
+    }
+
+    private static async Task HealthCheckShouldBeHealthyAsync(HttpResponseMessage healthCheckResponse, CancellationToken cancellationToken)
+    {
+        healthCheckResponse.Should().BeSuccessful();
+        (await healthCheckResponse.Content.ReadAsStringAsync(cancellationToken)).Should().Be("Healthy");
+    }
+
+    private static async Task LogsShouldNotContainWarningsAsync(IContainer container, CancellationToken cancellationToken)
+    {
+        (string Stdout, string Stderr) logValues = await container.GetLogsAsync(ct: cancellationToken);
+        Console.WriteLine($"Stderr:{Environment.NewLine}{logValues.Stderr}");
+        Console.WriteLine($"Stdout:{Environment.NewLine}{logValues.Stdout}");
+        logValues.Stdout.Should().NotContain("warn:");
+    }
 }
