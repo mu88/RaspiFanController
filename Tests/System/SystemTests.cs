a8ad48e468412ddcec8e450af33a3d393ce3c56a
mu88 <4560672+mu88@users.noreply.github.com>
2025-12-17 16:33:29 +0100

    ci: fix SBOM/provenance attestation and use new container base image approach

--- Tests/System/SystemTests.cs
+++ Tests/System/SystemTests.cs
@@ -54,10 +54,9 @@ public class SystemTests
                 Arguments =
                     $"publish {projectFile} --os linux --arch amd64 " +
                     $"/t:PublishContainersForMultipleFamilies " +
-                    $"-p:ReleaseVersion={containerImageTag} " +
-                    "-p:IsRelease=false " +
-                    "-p:ContainerRegistry=\"\" " + // image shall not be pushed
-                    "-p:ContainerRepository=\"me/raspifancontroller\" ",
+                    $"/p:ReleaseVersion={containerImageTag} " +
+                    "/p:IsRelease=false " +
+                    "/p:DoNotApplyGitHubScope=true", // ensures same behavior when run locally or in GitHub Actions
                 UseShellExecute = false,
                 RedirectStandardOutput = true,
                 CreateNoWindow = true
@@ -90,7 +89,7 @@ public class SystemTests
 
     private static IContainer BuildAppContainer(INetwork network, string containerImageTag) =>
         new ContainerBuilder()
-            .WithImage($"me/raspifancontroller:{containerImageTag}-chiseled")
+            .WithImage($"raspifancontroller:{containerImageTag}-chiseled")
             .WithNetwork(network)
             .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development") // this enables the faked temperature and fan controller as we're not on a real Raspi
             .WithPortBinding(8080, true)
